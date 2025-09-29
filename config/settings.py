707e93e9c847d2b9463b78e34bf5b7ca430b80d3
xofranc <santiagop200101@gmail.com>
2025-09-29 14:41:48 -0500

    Update settings to allow dynamic ALLOWED_HOSTS and restructure Reportes app

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +25,7 @@ SECRET_KEY = "django-insecure-rut5b%z$li&o=^u03rhgp#3dmtdkrj0gm8e7njk-v1a8=^z^xm
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Application definition
 
